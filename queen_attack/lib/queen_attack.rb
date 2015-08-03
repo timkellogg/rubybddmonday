f28a1ddf1f3bb3e2131c8b244ba16e3a177ecb50
tim kellogg & ryan heupler <rheupler@gmail.com>
2015-08-03 16:47:57 -0700

    handles vertical alignment

--- queen_attack/lib/queen_attack.rb
+++ queen_attack/lib/queen_attack.rb
@@ -1,7 +1,17 @@
 class Array
 
   define_method(:queen_attack?) do |coordinates|
-    
 
+    # check if array
+    if coordinates.is_a?(Array)
+      false
+    else
+      # check if horizontally aligned
+      if self[0] == coordinates[0]
+        true
+      else
+        false
+      end
+    end
   end
 end
--- queen_attack/spec/queen_attack_spec.rb
+++ queen_attack/spec/queen_attack_spec.rb
@@ -4,11 +4,13 @@ require('queen_attack')
 describe('Array#queen_attack?') do
 
   it('returns an error if the argument is not of type array') do
-    expect(([1,2].queen_attack?("bobsled_team")).to(eq("typeError: must be an array"))
+    expect([1,2].queen_attack?("bobsled_team")).to(eq(false))
   end
 
-  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+  it('is false if the coordinates are not vertically aligned') do
     expect([1,1].queen_attack?([2, 3])).to(eq(false))
   end
+
+
 
 end
